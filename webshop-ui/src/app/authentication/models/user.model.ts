2b18dbe13a1a7f42ff6a20bc78d1e593e786c1ce
Bedő Bence <bence9705@gmail.com>
2020-11-28 22:11:26 +0100

    load messages, send message implemented

--- webshop-ui/src/app/authentication/models/user.model.ts
+++ webshop-ui/src/app/authentication/models/user.model.ts
@@ -1,10 +1,12 @@
 import {Role} from '../enums/role.enum';
+import {Comment} from '../../caffposts/models/comment.module';
 
 export class User {
   id?: number;
   email: string;
   password: string;
   name?: string;
+  comments?: Comment[];
 
   roles?: { id: number, role: Role }[];
 }
--- webshop-ui/src/app/caffposts/components/caff-post-details/caff-post-details.component.ts
+++ webshop-ui/src/app/caffposts/components/caff-post-details/caff-post-details.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {map} from 'rxjs/operators';
-import {CaffPostsService} from "../../services/caff-posts.service";
-import {CaffPost} from "../../models/caff-post.model";
+import {CaffPostsService} from '../../services/caff-posts.service';
+import {CaffPost} from '../../models/caff-post.model';
+import {Comment} from '../../models/comment.module';
+import {AuthenticationService} from '../../../authentication/services/authentication.service';
 
 @Component({
   selector: 'app-caff-post-details',
@@ -11,8 +13,9 @@ import {CaffPost} from "../../models/caff-post.model";
 })
 export class CaffPostDetailsComponent implements OnInit {
 
-  testComments: string[] = ['wow', 'very comment', 'much wow', 'very big commentasdasdasdasdasdsadasd', 'kecske', 'heoheoheo sziasztok', ':))))))))))'];
+//  testComments: string[] = ['wow', 'very comment', 'much wow', 'very big commentasdasdasdasdasdsadasd', 'kecske', 'heoheoheo sziasztok', ':))))))))))'];
   selectedId: any;
+  comments: Comment[];
 
   public get selectedCaffPost(): Partial<CaffPost> {
     return this.caffPostsService.selectedCaffPost;
@@ -20,11 +23,22 @@ export class CaffPostDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private caffPostsService: CaffPostsService
+    private caffPostsService: CaffPostsService,
+    private authentiacionService: AuthenticationService
   ) {
     route.params.subscribe(params => {
-      this.selectedId = params['id'];
+      this.selectedId = params.id;
     });
+
+    caffPostsService.getComments(this.selectedId)
+        .subscribe(data => {
+          this.comments = [];
+          for (const id in data) {
+            if (data.hasOwnProperty(id)) {
+              this.comments.push(data[id]);
+            }
+          }
+        });
   }
 
   ngOnInit(): void {
@@ -35,6 +49,17 @@ export class CaffPostDetailsComponent implements OnInit {
   }
 
   public comment(comment: string): void {
-    console.log(comment);
+    this.caffPostsService.comment(this.selectedId, this.authentiacionService.loggedInUser.id, comment)
+      .subscribe(() => {
+        this.caffPostsService.getComments(this.selectedId)
+          .subscribe(data => {
+            this.comments = [];
+            for (const id in data) {
+              if (data.hasOwnProperty(id)) {
+                this.comments.push(data[id]);
+              }
+            }
+          });
+      });
   }
 }
